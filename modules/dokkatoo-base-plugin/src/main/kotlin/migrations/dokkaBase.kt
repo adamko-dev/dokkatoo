08a55434a51a7e995006b3f8a847799a6589ee4a
Adam <897017+aSemy@users.noreply.github.com>
2023-02-07 16:48:53 +0100

    comment out migrations

--- modules/dokkatoo-base-plugin/src/main/kotlin/migrations/dokkaBase.kt
+++ modules/dokkatoo-base-plugin/src/main/kotlin/migrations/dokkaBase.kt
@@ -2,6 +2,7 @@
 
 package org.jetbrains.dokka.base
 
+// temp disabled - maybe move migrations to a separate subproject?
 
-@Deprecated("TODO need to remove API dependency on Dokka. Maybe set up DSL for Dokka Generator plugins configuration?")
-object DokkaBase
+//@Deprecated("TODO need to remove API dependency on Dokka. Maybe set up DSL for Dokka Generator plugins configuration?")
+//object DokkaBase
--- modules/dokkatoo-base-plugin/src/main/kotlin/migrations/dokkaGradle.kt
+++ modules/dokkatoo-base-plugin/src/main/kotlin/migrations/dokkaGradle.kt
@@ -2,17 +2,19 @@
 
 package org.jetbrains.dokka.gradle
 
-import dev.adamko.dokkatoo.dokka.parameters.DokkaSourceSetGradleBuilder
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+// temp disabled - maybe move migrations to a separate subproject?
 
-//@Deprecated("dokka2")
-//typealias DokkaTask = org.jetbrains.dokka.gradle.tasks.DokkaConfigurationTask
-
-
-/**
- * Extension allowing configuration of Dokka source sets via Kotlin Gradle plugin source sets.
- */
-@Deprecated("dokkatoo...") // TODO what's the replacement...?
-fun DokkaSourceSetGradleBuilder.kotlinSourceSet(kotlinSourceSet: KotlinSourceSet) {
-  todoSourceSetName.set(kotlinSourceSet.name)
-}
+//import dev.adamko.dokkatoo.dokka.parameters.DokkaSourceSetGradleBuilder
+//import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+//
+////@Deprecated("dokka2")
+////typealias DokkaTask = org.jetbrains.dokka.gradle.tasks.DokkaConfigurationTask
+//
+//
+///**
+// * Extension allowing configuration of Dokka source sets via Kotlin Gradle plugin source sets.
+// */
+//@Deprecated("dokkatoo...") // TODO what's the replacement...?
+//fun DokkaSourceSetGradleBuilder.kotlinSourceSet(kotlinSourceSet: KotlinSourceSet) {
+//  todoSourceSetName.set(kotlinSourceSet.name)
+//}
