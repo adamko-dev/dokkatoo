8377ceaf9ba20d54394cdef65cd79d36d0228c6c
Adam <897017+aSemy@users.noreply.github.com>
2023-02-11 21:21:07 +0100

    re-introduce the kotlinSourceSet hack workaround

--- modules/dokkatoo-plugin/src/main/kotlin/migrations/dokkaGradle.kt
+++ modules/dokkatoo-plugin/src/main/kotlin/migrations/dokkaGradle.kt
@@ -2,6 +2,9 @@
 
 package org.jetbrains.dokka.gradle
 
+import dev.adamko.dokkatoo.dokka.parameters.DokkaSourceSetGradleBuilder
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
 // temp disabled - maybe move migrations to a separate subproject?
 
 //import dev.adamko.dokkatoo.dokka.parameters.DokkaSourceSetGradleBuilder
@@ -11,10 +14,10 @@ package org.jetbrains.dokka.gradle
 ////typealias DokkaTask = org.jetbrains.dokka.gradle.tasks.DokkaConfigurationTask
 //
 //
-///**
-// * Extension allowing configuration of Dokka source sets via Kotlin Gradle plugin source sets.
-// */
-//@Deprecated("dokkatoo...") // TODO what's the replacement...?
-//fun DokkaSourceSetGradleBuilder.kotlinSourceSet(kotlinSourceSet: KotlinSourceSet) {
-//  todoSourceSetName.set(kotlinSourceSet.name)
-//}
+/**
+ * Extension allowing configuration of Dokka source sets via Kotlin Gradle plugin source sets.
+ */
+@Deprecated("dokkatoo...") // TODO what's the replacement...?
+fun DokkaSourceSetGradleBuilder.kotlinSourceSet(kotlinSourceSet: KotlinSourceSet) {
+  todoSourceSetName.set(kotlinSourceSet.name)
+}
