c1bca7be1a28785fd0b2354a4e4da3bc08c83d25
Adam <897017+aSemy@users.noreply.github.com>
2023-02-15 23:33:40 +0100

    make PublicationPluginContext internal

--- modules/dokkatoo-plugin/src/main/kotlin/formats/DokkatooFormatPlugin.kt
+++ modules/dokkatoo-plugin/src/main/kotlin/formats/DokkatooFormatPlugin.kt
@@ -5,6 +5,7 @@ import dev.adamko.dokkatoo.DokkatooExtension
 import dev.adamko.dokkatoo.adapters.DokkatooJavaAdapter
 import dev.adamko.dokkatoo.adapters.DokkatooKotlinAdapter
 import dev.adamko.dokkatoo.dokka.DokkaPublication
+import dev.adamko.dokkatoo.internal.DokkatooInternalApi
 import dev.adamko.dokkatoo.internal.versions
 import javax.inject.Inject
 import org.gradle.api.Plugin
@@ -51,6 +52,10 @@ abstract class DokkatooFormatPlugin @Inject constructor(
     }
   }
 
+  /**
+   * Utility for adding dependencies to [org.gradle.api.artifacts.Configuration]s.
+   */
+  @DokkatooInternalApi
   class PublicationPluginContext(
     val project: Project,
     val dokkatooExtension: DokkatooExtension,
