5c065ecd14b1889d853a31b556098ab5e1efa9b8
Adam <897017+aSemy@users.noreply.github.com>
2023-02-17 11:06:30 +0100

    add code for includeBuild-ing the example projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -49,3 +49,45 @@ enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 //if (file("./examples/build/tmp/prepareDokkaSource").exists()) {
 //  includeBuild("./examples/build/tmp/prepareDokkaSource")
 //}
+
+// can only include one example project at a time https://github.com/gradle/gradle/issues/23939
+//@formatter:off
+//includeBuild(file("./examples/multiplatform-example/dokkatoo"                                                 )) { name = "multiplatform-example"          }
+//includeBuild(file("./examples/kotlin-as-java-example/dokkatoo"                                                )) { name = "kotlin-as-java-example"         }
+//includeBuild(file("./examples/versioning-multimodule-example/dokkatoo"                                        )) { name = "versioning-multimodule-example" }
+//includeBuild(file("./examples/custom-format-example/dokkatoo"                                                 )) { name = "custom-format-example"          }
+//includeBuild(file("./examples/gradle-example/dokkatoo"                                                        )) { name = "gradle-example"                 }
+//includeBuild(file("./examples/library-publishing-example/dokkatoo"                                            )) { name = "library-publishing-example"     }
+//includeBuild(file("./examples/multimodule-example/dokkatoo"                                                   )) { name = "multimodule-example"            }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-multimodule-1/dokkatoo"            )) { name = "it-multimodule-1"               }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-multimodule-0/dokkatoo"            )) { name = "it-multimodule-0"               }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-collector-0/dokkatoo"              )) { name = "it-collector-0"                 }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-multimodule-versioning-0/dokkatoo" )) { name = "it-multimodule-versioning-0"    }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-android-0/dokkatoo"                )) { name = "it-android-0"                   }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-basic/dokkatoo"                    )) { name = "it-basic"                       }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-multiplatform-0/dokkatoo"          )) { name = "it-multiplatform-0"             }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-js-ir-0/dokkatoo"                  )) { name = "it-js-ir-0"                     }
+//includeBuild(file("./modules/dokkatoo-plugin-integration-tests/projects/it-basic-groovy/dokkatoo"             )) { name = "it-basic-groovy"                }
+//@formatter:on
+
+//listOf(
+//  "examples",
+//  "modules/dokkatoo-plugin-integration-tests/projects",
+//).forEach { exampleProjectDir ->
+//  file(exampleProjectDir)
+//    .walk()
+//    .filter {
+//      it.isDirectory
+//          && it.name == "dokkatoo"
+//          && (
+//          it.resolve("settings.gradle.kts").exists()
+//              ||
+//              it.resolve("settings.gradle").exists()
+//          )
+//    }.forEach { file ->
+//      includeBuild(file) {
+//        name = file.parentFile.name
+//        println("$file $name")
+//      }
+//    }
+//}
