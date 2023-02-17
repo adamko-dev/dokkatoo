f749feeb84f5cfce20fe519cd9cecd2b7d3b10f7
Adam <897017+aSemy@users.noreply.github.com>
2023-02-17 14:56:00 +0100

    try fixing testMavenRepo on Windows

--- examples/multimodule-example/dokkatoo/settings.gradle.kts
+++ examples/multimodule-example/dokkatoo/settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
   repositories {
     gradlePluginPortal()
     mavenCentral()
-    maven(providers.gradleProperty("testMavenRepo").map { rootDir.resolve(it) })
+    maven(providers.gradleProperty("testMavenRepo").map(::file))
   }
 }
 
@@ -15,7 +15,7 @@ pluginManagement {
 dependencyResolutionManagement {
   repositories {
     mavenCentral()
-    maven(providers.gradleProperty("testMavenRepo").map { rootDir.resolve(it) })
+    maven(providers.gradleProperty("testMavenRepo").map(::file))
   }
 }
 
--- modules/dokkatoo-plugin-integration-tests/projects/it-multimodule-0/dokkatoo/settings.gradle.kts
+++ modules/dokkatoo-plugin-integration-tests/projects/it-multimodule-0/dokkatoo/settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
   repositories {
     gradlePluginPortal()
     mavenCentral()
-    maven(providers.gradleProperty("testMavenRepo").map { rootDir.resolve(it) })
+    maven(providers.gradleProperty("testMavenRepo").map(::file))
   }
 }
 
@@ -12,6 +12,6 @@ pluginManagement {
 dependencyResolutionManagement {
   repositories {
     mavenCentral()
-    maven(providers.gradleProperty("testMavenRepo").map { rootDir.resolve(it) })
+    maven(providers.gradleProperty("testMavenRepo").map(::file))
   }
 }
