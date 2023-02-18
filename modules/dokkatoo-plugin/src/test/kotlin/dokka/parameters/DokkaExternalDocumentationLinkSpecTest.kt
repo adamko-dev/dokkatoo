dd33200a7175cb79778c83f8b9c4231af7b00f11
Adam <897017+aSemy@users.noreply.github.com>
2023-02-18 16:32:55 +0100

    additional DokkaExternalDocumentationLinkSpec tests

--- modules/dokkatoo-plugin/src/test/kotlin/dokka/parameters/DokkaExternalDocumentationLinkSpecTest.kt
+++ modules/dokkatoo-plugin/src/test/kotlin/dokka/parameters/DokkaExternalDocumentationLinkSpecTest.kt
@@ -1,22 +1,23 @@
 package dev.adamko.dokkatoo.dokka.parameters
 
 import dev.adamko.dokkatoo.create_
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import java.net.URL
+import org.gradle.api.Project
+import org.gradle.api.internal.provider.MissingValueException
 import org.gradle.kotlin.dsl.domainObjectContainer
 import org.gradle.testfixtures.ProjectBuilder
 
 
 class DokkaExternalDocumentationLinkSpecTest : FunSpec({
+  val project = ProjectBuilder.builder().build()
 
   context("expect url can be set") {
     test("using a string") {
-      val project = ProjectBuilder.builder().build()
-
-      val container = project.objects
-        .domainObjectContainer(DokkaExternalDocumentationLinkSpec::class)
-
-      val actual = container.create_("test") {
+      val actual = project.createExternalDocLinkSpec("test") {
         url("https://github.com/adamko-dev/dokkatoo/")
       }
 
@@ -24,12 +25,7 @@ class DokkaExternalDocumentationLinkSpecTest : FunSpec({
     }
 
     test("using a string-provider") {
-      val project = ProjectBuilder.builder().build()
-
-      val container = project.objects
-        .domainObjectContainer(DokkaExternalDocumentationLinkSpec::class)
-
-      val actual = container.create_("test") {
+      val actual = project.createExternalDocLinkSpec("test") {
         url(project.provider { "https://github.com/adamko-dev/dokkatoo/" })
       }
 
@@ -39,12 +35,7 @@ class DokkaExternalDocumentationLinkSpecTest : FunSpec({
 
   context("expect packageListUrl can be set") {
     test("using a string") {
-      val project = ProjectBuilder.builder().build()
-
-      val container = project.objects
-        .domainObjectContainer(DokkaExternalDocumentationLinkSpec::class)
-
-      val actual = container.create_("test") {
+      val actual = project.createExternalDocLinkSpec("test") {
         packageListUrl("https://github.com/adamko-dev/dokkatoo/")
       }
 
@@ -52,16 +43,46 @@ class DokkaExternalDocumentationLinkSpecTest : FunSpec({
     }
 
     test("using a string-provider") {
-      val project = ProjectBuilder.builder().build()
-
-      val container = project.objects
-        .domainObjectContainer(DokkaExternalDocumentationLinkSpec::class)
-
-      val actual = container.create_("test") {
+      val actual = project.createExternalDocLinkSpec("test") {
         packageListUrl(project.provider { "https://github.com/adamko-dev/dokkatoo/" })
       }
 
       actual.packageListUrl.get().toString() shouldBe "https://github.com/adamko-dev/dokkatoo/"
     }
   }
+
+  context("when building a ExternalDocumentationLinkKxs") {
+    test("expect url is required") {
+      val actual = project.createExternalDocLinkSpec("test") {
+        url.set(null as URL?)
+        packageListUrl("https://github.com/adamko-dev/dokkatoo/")
+      }
+
+      val caughtException = shouldThrow<MissingValueException> {
+        actual.build()
+      }
+
+      caughtException.message shouldContain "Cannot query the value of property 'url' because it has no value available"
+    }
+    test("expect packageListUrl is required") {
+      val actual = project.createExternalDocLinkSpec("test") {
+        url("https://github.com/adamko-dev/dokkatoo/")
+        packageListUrl.set(null as URL?)
+      }
+
+      val caughtException = shouldThrow<MissingValueException> {
+        actual.build()
+      }
+
+      caughtException.message shouldContain "Cannot query the value of property 'packageListUrl' because it has no value available"
+    }
+  }
 })
+
+private fun Project.createExternalDocLinkSpec(
+  name: String,
+  configure: DokkaExternalDocumentationLinkSpec.() -> Unit
+): DokkaExternalDocumentationLinkSpec =
+  objects
+    .domainObjectContainer(DokkaExternalDocumentationLinkSpec::class)
+    .create_(name, configure)
