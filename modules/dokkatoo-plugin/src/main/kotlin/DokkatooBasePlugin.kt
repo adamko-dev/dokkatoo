f02dae42a1cf08c27e3659e78c093807923a4dfa
Adam <897017+aSemy@users.noreply.github.com>
2023-02-19 18:24:11 +0100

    add HeapDumpOnOutOfMemoryError option

--- modules/dokkatoo-plugin/src/main/kotlin/DokkatooBasePlugin.kt
+++ modules/dokkatoo-plugin/src/main/kotlin/DokkatooBasePlugin.kt
@@ -97,7 +97,14 @@ abstract class DokkatooBasePlugin @Inject constructor(
       // increase memory - DokkaGenerator is hungry https://github.com/Kotlin/dokka/issues/1405
       workerMinHeapSize.convention("256m")
       workerMaxHeapSize.convention("1g")
-      workerJvmArgs.set(listOf("-XX:MaxMetaspaceSize=512m"))
+      workerJvmArgs.set(
+        listOf(
+          "-XX:MaxMetaspaceSize=512m",
+          "-XX:+HeapDumpOnOutOfMemoryError",
+          //"-XX:StartFlightRecording=disk=true,name={path.drop(1).map { if (it.isLetterOrDigit()) it else '-' }.joinToString("")},dumponexit=true,duration=30s",
+          //"-XX:FlightRecorderOptions=repository=$baseDir/jfr,stackdepth=512",
+        )
+      )
     }
 
     target.tasks.withType<DokkatooPrepareModuleDescriptorTask>().all task@{
