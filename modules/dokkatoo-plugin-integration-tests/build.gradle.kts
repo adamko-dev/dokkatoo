056167763b5b6392dd485fe7f0e6f652b83e5f23
Adam <897017+aSemy@users.noreply.github.com>
2023-02-20 20:59:52 +0100

    try disabling test increased memory and forking...

--- modules/dokkatoo-plugin-integration-tests/build.gradle.kts
+++ modules/dokkatoo-plugin-integration-tests/build.gradle.kts
@@ -203,11 +203,11 @@ tasks.setupDokkaTemplateProjects {
   )
 }
 
-tasks.withType<Test>().configureEach {
-  // this seems to help OOM errors in the Worker Daemons
-  setForkEvery(1)
-  jvmArgs(
-    "-Xmx1g",
-    "-XX:MaxMetaspaceSize=512m",
-  )
-}
+//tasks.withType<Test>().configureEach {
+//  // this seems to help OOM errors in the Worker Daemons
+//  setForkEvery(1)
+//  jvmArgs(
+//    "-Xmx1g",
+//    "-XX:MaxMetaspaceSize=512m",
+//  )
+//}
