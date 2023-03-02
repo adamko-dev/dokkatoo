73726ee9eee72b9be93d72589e69df1049725c9a
Adam <897017+aSemy@users.noreply.github.com>
2023-03-02 12:43:12 +0100

    fix line endings in template projects

--- modules/dokkatoo-plugin-integration-tests/projects/it-basic/dokkatoo/build.gradle.kts
+++ modules/dokkatoo-plugin-integration-tests/projects/it-basic/dokkatoo/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.dokka.gradle.kotlinSourceSet
 
 plugins {
   kotlin("jvm") version "1.7.20"
-  id("dev.adamko.dokkatoo") version "0.0.2-SNAPSHOT"
+  id("dev.adamko.dokkatoo") version "0.0.3-SNAPSHOT"
 }
 
 version = "1.7.20-SNAPSHOT"
