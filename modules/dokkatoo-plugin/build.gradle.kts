1173bddffa381c65f205938853057320467b78ef
Adam <897017+aSemy@users.noreply.github.com>
2023-03-12 19:44:47 +0100

    update dependencies to use Gradle version catalog (plus some build script tidying)

--- modules/dokkatoo-plugin/build.gradle.kts
+++ modules/dokkatoo-plugin/build.gradle.kts
@@ -21,25 +21,30 @@ plugins {
 
 description = "Generates documentation for Kotlin projects (using Dokka)"
 
-val dokkaVersion = provider { "1.7.20" }
-
 dependencies {
-  implementation(dokkaVersion.map { "org.jetbrains.dokka:dokka-core:$it" })
 
-  compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-  compileOnly("com.android.tools.build:gradle:4.0.1")
+  implementation(libs.kotlin.dokkaCore)
 
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+  compileOnly(libs.gradlePlugin.kotlin)
+  compileOnly(libs.gradlePlugin.android)
+
+  implementation(platform(libs.kotlinxSerialization.bom))
+  implementation(libs.kotlinxSerialization.json)
 
   testFixturesImplementation(gradleApi())
   testFixturesImplementation(gradleTestKit())
-  testFixturesCompileOnly(dokkaVersion.map { "org.jetbrains.dokka:dokka-core:$it" })
-  testFixturesImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-  testFixturesApi(platform("io.kotest:kotest-bom:5.5.5"))
-  testFixturesApi("io.kotest:kotest-runner-junit5")
-  testFixturesApi("io.kotest:kotest-assertions-core")
-  testFixturesApi("io.kotest:kotest-assertions-json")
-  testFixturesApi("io.kotest:kotest-framework-datatest")
+
+  testFixturesCompileOnly(libs.kotlin.dokkaCore)
+  testFixturesImplementation(platform(libs.kotlinxSerialization.bom))
+  testFixturesImplementation(libs.kotlinxSerialization.json)
+
+  testFixturesCompileOnly(libs.kotlin.dokkaCore)
+
+  testFixturesApi(platform(libs.kotest.bom))
+  testFixturesApi(libs.kotest.junit5Runner)
+  testFixturesApi(libs.kotest.assertionsCore)
+  testFixturesApi(libs.kotest.assertionsJson)
+  testFixturesApi(libs.kotest.datatest)
 
   // don't define test dependencies here, instead define them in the testing.suites {} configuration below
 }
@@ -107,14 +112,15 @@ testing.suites {
     dependencies {
       implementation(project.dependencies.gradleTestKit())
 
-      implementation("org.jetbrains.kotlin:kotlin-test:1.7.20")
+      implementation(project.dependencies.testFixtures(project()))
 
-      implementation(project.dependencies.testFixtures(project))
+      compileOnly(libs.kotlin.dokkaCore)
+//      runtimeOnly(libs.kotlin.dokkaCore)
 
-      implementation("org.jetbrains.dokka:dokka-core:1.7.20")
-      implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+      implementation(project.dependencies.platform(libs.kotlinxSerialization.bom))
+      implementation(libs.kotlinxSerialization.json)
 
-      runtimeOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:$embeddedKotlinVersion")
+//      compileOnly(libs.kotlin.dokkaCore)
     }
 
     targets.configureEach {
@@ -152,6 +158,10 @@ testing.suites {
   val testFunctional by registering(JvmTestSuite::class) {
     description = "Tests that use Gradle TestKit to test functionality"
     testType.set(TestSuiteType.FUNCTIONAL_TEST)
+
+//    dependencies {
+//      implementation(libs.kotlinxSerialization.json)
+//    }
 
     targets.all {
       testTask.configure {
@@ -204,7 +214,7 @@ val generateDokkatooConstants by tasks.registering(Sync::class) {
 
   val textResources = resources.text
   val dokkatooVersion = dokkatooVersion
-  val dokkaVersion = dokkaVersion
+  val dokkaVersion = libs.versions.kotlin.dokka
 
   val properties = objects.mapProperty<String, String>().apply {
     put("DOKKATOO_VERSION", dokkatooVersion)
