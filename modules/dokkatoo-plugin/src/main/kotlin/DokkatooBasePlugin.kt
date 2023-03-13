22523024ea68b5d338a2a54f98b70cfa2cfaa765
Adam <897017+aSemy@users.noreply.github.com>
2023-03-13 13:39:05 +0100

    add AlwaysPreTouch everywhere

--- modules/dokkatoo-plugin/src/main/kotlin/DokkatooBasePlugin.kt
+++ modules/dokkatoo-plugin/src/main/kotlin/DokkatooBasePlugin.kt
@@ -79,6 +79,7 @@ constructor(
         listOf(
           //"-XX:MaxMetaspaceSize=512m",
           "-XX:+HeapDumpOnOutOfMemoryError",
+          "-XX:+AlwaysPreTouch", // https://github.com/gradle/gradle/issues/3093#issuecomment-387259298
           //"-XX:StartFlightRecording=disk=true,name={path.drop(1).map { if (it.isLetterOrDigit()) it else '-' }.joinToString("")},dumponexit=true,duration=30s",
           //"-XX:FlightRecorderOptions=repository=$baseDir/jfr,stackdepth=512",
         )
