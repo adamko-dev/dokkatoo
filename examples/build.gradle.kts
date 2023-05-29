5fe9e0c67812f728ed1f84a7225893dfc500451f
Adam <897017+aSemy@users.noreply.github.com>
2023-05-29 07:48:52 +0200

    tidy `examples/build.gradle.kts`

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -34,22 +34,6 @@ dokkaTemplateProjects {
   )
 }
 
-//tasks.setupDokkaTemplateProjects {
-//  destinationToSources.set(
-//    mapOf(
-//      //@formatter:off
-//      "custom-format-example/dokka"          to listOf("examples/gradle/dokka-customFormat-example"),
-//      "gradle-example/dokka"                 to listOf("examples/gradle/dokka-gradle-example"),
-//      "kotlin-as-java-example/dokka"         to listOf("examples/gradle/dokka-kotlinAsJava-example"),
-//      "library-publishing-example/dokka"     to listOf("examples/gradle/dokka-library-publishing-example"),
-//      "multimodule-example/dokka"            to listOf("examples/gradle/dokka-multimodule-example"),
-//      "multiplatform-example/dokka"          to listOf("examples/gradle/dokka-multiplatform-example"),
-//      "versioning-multimodule-example/dokka" to listOf("examples/gradle/dokka-versioning-multimodule-example"),
-//      //@formatter:on
-//    ).mapKeys { (dest, _) -> projectDir.resolve(dest) }
-//  )
-//}
-
 configurations.exampleProjectsElements.configure {
   outgoing {
     artifact(projectDir) {
