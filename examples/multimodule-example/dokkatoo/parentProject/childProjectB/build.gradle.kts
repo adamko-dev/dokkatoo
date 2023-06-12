ea7a4505bbd2915f04c7116b1361d257df3b28cf
Adam <897017+aSemy@users.noreply.github.com>
2023-06-12 14:03:46 +0200

    implement Dokkatoo convention plugin in multiplatform-example

--- examples/multimodule-example/dokkatoo/parentProject/childProjectB/build.gradle.kts
+++ examples/multimodule-example/dokkatoo/parentProject/childProjectB/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
   kotlin("jvm")
-  id("dev.adamko.dokkatoo") version "1.5.0-SNAPSHOT"
+  `dokka-convention`
 }
 
 dokkatoo {
   dokkatooSourceSets.configureEach {
     includes.from("ModuleB.md")
   }
-  modulePath.set("childProjectB") // match the original dokka default
 }
 
 //region DON'T COPY - this is only needed for internal Dokkatoo integration tests
+dokkatoo {
+  modulePath.set("childProjectB") // match the original dokka default
+}
 tasks.withType<dev.adamko.dokkatoo.tasks.DokkatooGenerateTask>().configureEach {
   generator.dokkaSourceSets.configureEach {
     sourceSetScope.set(":parentProject:childProjectB:dokkaHtmlPartial")
