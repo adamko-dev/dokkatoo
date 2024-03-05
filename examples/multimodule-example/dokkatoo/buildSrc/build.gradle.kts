99fcb9a1d678a749db5f5ee57791ccaa25a93754
Adam <897017+aSemy@users.noreply.github.com>
2024-03-05 10:27:15 +0100

    release 2.2.0

--- examples/multimodule-example/dokkatoo/buildSrc/build.gradle.kts
+++ examples/multimodule-example/dokkatoo/buildSrc/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-  implementation("dev.adamko.dokkatoo:dokkatoo-plugin:2.2.0-SNAPSHOT")
+  implementation("dev.adamko.dokkatoo:dokkatoo-plugin:2.2.0")
 }
