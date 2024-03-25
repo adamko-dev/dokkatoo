e31cbc1c19cfd2b29b16ea70d8dd05ebf9922289
Adam <897017+aSemy@users.noreply.github.com>
2024-03-25 11:04:30 +0100

    release 2.3.0 (#205)

--- examples/multiplatform-example/dokkatoo/build.gradle.kts
+++ examples/multiplatform-example/dokkatoo/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   kotlin("multiplatform") version "1.9.22"
-  id("dev.adamko.dokkatoo") version "2.3.0-SNAPSHOT"
+  id("dev.adamko.dokkatoo") version "2.3.0"
 }
 
 group = "org.dokka.example"
