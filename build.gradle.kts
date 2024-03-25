787e704eb4b84c01ad79ba7daeb3526d0f19b38d
Adam <897017+aSemy@users.noreply.github.com>
2024-03-25 11:30:31 +0100

    release 2.4.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "dev.adamko.dokkatoo"
-version = "2.3.0"
+version = "2.4.0-SNAPSHOT"
 
 
 idea {
--- examples/multimodule-example/dokkatoo/buildSrc/build.gradle.kts
+++ examples/multimodule-example/dokkatoo/buildSrc/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-  implementation("dev.adamko.dokkatoo:dokkatoo-plugin:2.3.0")
+  implementation("dev.adamko.dokkatoo:dokkatoo-plugin:2.4.0-SNAPSHOT")
 }
